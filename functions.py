cef967c18d0853a4bdb53070c6532937290939af
Poorna-A <apurna1998@gmail.com>
2021-02-27 12:10:01 +0530

    calc as module

---
+++ functions.py
@@ -0,0 +1,13 @@
+from calcmodule import add,sub,mul,div
+a=int(input("enter two numbers"))
+b=int(input("enter a num"))
+
+
+addresult=add(a,b)
+print(addresult)
+result=sub(a,b)
+print(result)
+mulresult=mul(a,b)
+print(mulresult)
+divresult=div(a,b)
+print(divresult)
